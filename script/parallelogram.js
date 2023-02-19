d587d8cea70dbfe3eb8ef21dfc8b7cda085d6043
Amiodas <amio.das.dip@gmail.com>
2023-02-19 15:34:57 +0000

    #parallelogram area calculation

---
+++ script/parallelogram.js
@@ -0,0 +1,14 @@
+console.log("parallelogram");
+
+const paralleogramBase = document.getElementById("parallelogram-base");
+const paralleogramHeight = document.getElementById("parallelogram-height");
+const parallelogramCalculate = document.getElementById(
+  "parallelogram-area-calculation"
+);
+
+parallelogramCalculate.addEventListener("click", function () {
+  const areaOfparallelogram =
+    parseFloat(paralleogramBase.value) * parseFloat(paralleogramHeight.value);
+  console.log(paralleogramBase.value, paralleogramHeight.value, areaOfparallelogram);
+  showResult("Parallelogram", areaOfparallelogram);
+});
